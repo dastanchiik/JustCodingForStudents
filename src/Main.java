1e11d0d38db9ef539248676b4f97ea5ad638a8e6
dastanchiik <dosyafree32@gmail.com>
2025-02-21 09:39:17 +0600

    switch case not

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String str = scanner.next();
-        switch (str){
-            case "January":
-                System.out.println("Январь");
-                break;
-            case "February":
-                System.out.println("Февраль");
-                break;
-            default:
-                System.out.println("Мындай Ай Жок");
-        }
+
     }
 }
