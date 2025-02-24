508926582defc30b213b9195da6eb8f6a12ad89e
dastanchiik <dosyafree32@gmail.com>
2025-02-24 09:54:38 +0600

    return method

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(method());
+    }
+    public static int method(){
+        int a = 15;
+        return a;
     }
 }
